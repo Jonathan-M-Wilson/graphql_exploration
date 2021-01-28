6a262ecf22bb44097653e5fa2e991f905cbb747c
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-28 14:17:26 -0700

    Complete graphql exploration

---
+++ server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Initialize a GraphQL schema <- probably what we were looking for with graphlient
+var schema = buildSchema(`
+  type Query {
+    user(id: Int!): Person
+    users(shark: String): [Person]
+  },
+  type Person {
+    id: Int
+    name: String
+    age: Int
+    shark: String
+  }
+  type Mutation {
+    updateUser(id: Int!, name: String!, age: String): Person
+  }
+  `);
+
+// Sample users
+var users = [
+  {
+    id: 1,
+    name: 'Brian',
+    age: '21',
+    shark: 'Great White Shark'
+  },
+  {
+    id: 2,
+    name: 'Kim',
+    age: '22',
+    shark: 'Whale Shark'
+  },
+  {
+    id: 3,
+    name: 'Faith',
+    age: '23',
+    shark: 'Hammerhead Shark'
+  },
+  {
+    id: 4,
+    name: 'Jospeh',
+    age: '23',
+    shark: 'Tiger Shark'
+  },
+  {
+    id: 5,
+    name: 'Joy',
+    age: '25',
+    shark: 'Hammerhead Shark'
+  }
+];
+
+var getUser = function(args) {
+  var userId = args.id;
+  return users.filter(user => user.id == userId)[0];
+}
+
+var retrieveUsers = function(args) {
+  if (args.shark) {
+    var shark = args.shark;
+    return users.filter(user => user.shark === shark);
+  } else {
+    return users;
+  }
+}
+
+var updateUser = function({id, name, age}) {
+  users.map(user => {
+    if (user.id === id) {
+      user.name = name;
+      user.age = age;
+      return user;
+    }
+  });
+  return users.filter(user => user.id === id)[0];
+}
+
+// Root resolver
+var root = {
+  user: getUser,
+  users: retrieveUsers,
+  updateUser: updateUser
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(3000, () => console.log('Now browse to localhost:3000/graphql'));
